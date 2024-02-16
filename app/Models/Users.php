b7cf09ebfcc2680a8212e16c64e111b667f589f0
Anuskota <anamangada.dev@gmail.com>
2024-02-16 16:41:42 +0100

    chenges

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -5,26 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Users extends Model
+class User extends Model
 {
     use HasFactory;
 
-    protected $table = 'usuarios';
-
     protected $fillable = [
-        'nombre',
-        'correo',
-        'palabra_secreta',
-        'administrador',
+        'name',
+        'email',
+        'password',
+        'admin',
     ];
 
-    public function archivos()
-    {
-        return $this->hasMany(Files::class, 'id_usuario');
-    }
-
-    public function archivosCompartidos()
-    {
-        return $this->hasMany(ArchivoCompartido::class, 'id_usuario_compartido');
-    }
+    protected $casts = [
+        'admin' => 'boolean',
+    ];
 }
